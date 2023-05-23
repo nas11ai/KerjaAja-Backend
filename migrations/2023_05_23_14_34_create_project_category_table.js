c3e033aeb21ecfec75b2b80c50e4063d849f9eee
nas11ai <kairupannasai@gmail.com>
2023-05-23 14:46:48 +0800

    feat: init project category model

---
+++ migrations/2023_05_23_14_34_create_project_category_table.js
@@ -0,0 +1,54 @@
+const DataTypes = require("sequelize");
+
+module.exports = {
+  up: async ({ context: queryInterface }) => {
+    await queryInterface.createTable("project_categories", {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        unique: {
+          args: true,
+          msg: 'Project Category ID has been taken',
+        },
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: 'Project Category ID can\'t be empty',
+          },
+        },
+      },
+      name: {
+        type: DataTypes.STRING,
+        unique: {
+          args: true,
+          msg: 'Project Category name has been taken',
+        },
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: 'Please enter the Project Category name',
+          },
+        },
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+    }, {
+      uniqueKeys: {
+        unique_tag: {
+          customIndex: true,
+          fields: ["id", "name"],
+        },
+      },
+    });
+  },
+  down: async ({ context: queryInterface }) => {
+    await queryInterface.dropTable("project_categories");
+  },
+};
