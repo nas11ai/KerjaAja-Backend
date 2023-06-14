0f42b9f8663e7d03b54eeb905180d4e0f669df61
Annisa Syafiqah <annisa.syafiqah02@gmail.com>
2023-06-14 19:51:47 +0700

    fix: saving online user's Id using object

--- chat-feature/index.js
+++ chat-feature/index.js
@@ -9,17 +9,17 @@ module.exports = (io) => {
     res.send("Chat is running");
     next();
   });
-  
+
   const chatIo = io.of('/chat-feature');
-  var users = [];
-  
+  let users = {};
+
   chatIo.on('connection', (socket) => {
     console.log('User connected', socket.id);
-    
+
     socket.on('userConnected', (userId) => {
       const socketId = socket.id;
       users[userId] = socketId;
-      
+
       redisClient.hmset("online_users", userId, socketId, (err, reply) => {
         if (err) {
           console.error('Gagal menyimpan data user online:', err);
@@ -28,8 +28,11 @@ module.exports = (io) => {
 
       chatIo.emit('userConnected', userId);
     });
-  
-    socket.on('getMessages', ({ receiver, sender }, callback) => {
+
+    socket.on('getMessages', ({
+      receiver,
+      sender
+    }, callback) => {
 
 
       redisClient.lrange(`sender_${sender}:${receiver}:messages`, 0, -1, (err, senderToReceiverMessages) => {
@@ -49,23 +52,27 @@ module.exports = (io) => {
           const allMessages = SenderToReceiver.concat(ReceiverToSender);
           const sortedMessages = allMessages.sort((a, b) => a.timestamp - b.timestamp);
           callback(sortedMessages);
-
+          console.log(sortedMessages);
         });
       });
     });
-  
+
     socket.on("sendMessage", (data) => {
-      const { sender, receiver, message } = data;
+      const {
+        sender,
+        receiver,
+        message
+      } = data;
       const messageData = {
         sender,
         message,
         timestamp: Date.now()
       };
-      
+
       if (users.hasOwnProperty(receiver)) {
         const socketId = users[receiver];
         chatIo.to(socketId).emit("newMessage", data);
-        
+
       }
 
       redisClient.rpush(`sender_${sender}:${receiver}:messages`, JSON.stringify(messageData), (err) => {
@@ -73,50 +80,65 @@ module.exports = (io) => {
           console.error(err);
         }
       });
-  
+
       redisClient.hincrby(`sender_:${sender}:unread`, `${receiver}:unread_count`, 1, (err) => {
         if (err) {
           console.error('Gagal menambahkan unread count:', err);
         }
       });
     });
-  
+
     socket.on('messageRead', (data) => {
-      const { sender, receiver } = data;
+      const {
+        sender,
+        receiver
+      } = data;
       redisClient.hset(`sender:${sender}:unread`, `${receiver}:unread_count`, 0, (err) => {
         if (err) {
           console.error('Gagal menghapus unread :', err);
         }
         const channel = `sender:${receiver}:receiver`;
-        chatIo.to(channel).emit('messageRead', { sender, receiver });
+        chatIo.to(channel).emit('messageRead', {
+          sender,
+          receiver
+        });
       });
     });
-  
+
     socket.on('getUnreadMessages', (data) => {
-      const { sender, receiver } = data;
+
+      const {
+        sender,
+        receiver
+      } = data;
+
       redisClient.hget(`sender_:${sender}:unread`, `${receiver}:unread_count`, (err, count) => {
         if (err) {
           console.error('Gagal mengambil jumlah pesan yang belum dibaca:', err);
         }
         const channel = `sender:${sender}:receiver`;
-        io.to(channel).emit('unreadCount', { count });
+        io.to(channel).emit('unreadCount', {
+          count
+        });
       });
     });
-  
-    socket.on('disconnected', () => {
+
+    socket.on('disconnect', () => {
+
       const userId = Object.keys(users).find(key => users[key] === socket.id);
 
-      // Hapus data pengguna online dari Redis menggunakan userId sebagai kunci
       if (userId) {
-        redisClient.hdel("online_users", userId, (err) => {
+        delete users[userId];
+        redisClient.hdel("online_users", userId, (err, reply) => {
           if (err) {
             console.error('Gagal menghapus data pengguna online:', err);
+          } else {
+            console.log('Data pengguna online dihapus:', reply);
           }
         });
       }
     });
   });
 
- 
-return router;
+  return router;
 };
